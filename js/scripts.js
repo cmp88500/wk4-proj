a027161fbe99fd472034fe491d514ee7018f94eb
pchmc72462 <pchmc72462@gmail.com>
2018-08-24 10:43:12 -0700

    Create UI logic in Javascript. Still a work in progress.

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,15 @@ var pepperoni = new Pizza ("large", ["cheese", "pepperoni"], 6.00);
 var meats = new Pizza ("large", ["cheese", "pepperoni", "sausage", "bacon"], 8.00);
 var supreme = new Pizza ("large", ["cheese", "pepperoni", "sausage", "bacon", "green peppers", "mushrooms", "olives"], 8.50);
 
+// USER INTERFACE LOGIC
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    $("#review").prepend("<li><span class='display'>" + Pizza.size + "</span></li>");
+    $("#review").after("<li><span class='display'>" + Pizza.toppings + "</span></li>");
+    $("#review").after("<li><span class='display' id='price'" + Pizza.priceSet() + "</span></li>");
+})
+})
 
 // console.log(cheese);
 // console.log(meats.toppings);
