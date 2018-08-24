2627c7f9b244eaa8869b42bd01affc5e23d9b7d8
pchmc72462 <pchmc72462@gmail.com>
2018-08-24 09:20:30 -0700

    Declare prototype that rounds price to two decimal places, and displays in currency.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,10 @@ function Pizza (size, toppings, price) {
   this.price = price;
 }
 
+Pizza.prototype.priceSet = function() {
+  return "$" + this.price.toFixed(2)
+}
+
 var cheese = new Pizza ("large", ["cheese"], 5.00);
 var pepperoni = new Pizza ("large", ["cheese", "pepperoni"], 6.00);
 var meats = new Pizza ("large", ["cheese", "pepperoni", "sausage", "bacon"], 8.00);
@@ -12,3 +16,4 @@ var supreme = new Pizza ("large", ["cheese", "pepperoni", "sausage", "bacon", "g
 console.log(cheese);
 console.log(meats.toppings);
 console.log(pepperoni.price);
+console.log(pepperoni.priceSet());
