53005885c6a913efc6dd889e90abfc58748b12d4
pchmc72462 <pchmc72462@gmail.com>
2018-08-24 09:04:53 -0700

    Change crust key-value pair to price

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
-function Pizza (size, toppings, crust) {
+function Pizza (size, toppings, price) {
   this.size = size;
   this.toppings = toppings;
-  this.crust = crust;
+  this.price = price;
 }
 
-var cheese = new Pizza ("large", ["cheese"], "pan");
-var pepperoni = new Pizza ("large", ["cheese", "pepperoni"], "pan");
-var meats = new Pizza ("large", ["cheese", "pepperoni", "sausage", "bacon"], "pan");
-var supreme = new Pizza ("large", ["cheese", "pepperoni", "sausage", "bacon", "green peppers", "mushrooms", "olives"], "pan");
+var cheese = new Pizza ("large", ["cheese"], 5.00);
+var pepperoni = new Pizza ("large", ["cheese", "pepperoni"], 6.00);
+var meats = new Pizza ("large", ["cheese", "pepperoni", "sausage", "bacon"], 8.00);
+var supreme = new Pizza ("large", ["cheese", "pepperoni", "sausage", "bacon", "green peppers", "mushrooms", "olives"], 8.50);
 
 console.log(cheese);
 console.log(meats.toppings);
+console.log(pepperoni.price);
