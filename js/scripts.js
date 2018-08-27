4394c0bd89737460ebfd62b143d4d0984465fc85
pchmc72462 <pchmc72462@gmail.com>
2018-08-26 21:25:18 -0700

    Change prototype to calculate price by pizza size.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,37 @@
 // BUSINESS LOGIC
-function Pizza (size, crust, sauce, toppings) {
+function Pizza (size) {
   this.size = size;
-  this.crust = crust;
-  this.toppings = toppings;
-  this.sauce = sauce;
+  // this.crust = crust;
+  // this.toppings = toppings;
+  // this.sauce = sauce;
 }
 
-var cheese = new Pizza ("large", ["cheese"], 5.00);
-var pepperoni = new Pizza ("large", ["cheese", "pepperoni"], 6.00);
-var meats = new Pizza ("large", ["cheese", "pepperoni", "sausage", "bacon"], 8.00);
-var supreme = new Pizza ("large", ["cheese", "pepperoni", "sausage", "bacon", "green peppers", "mushrooms", "olives"], 8.50);
-var custom = new Pizza ("large", ["cheese"], 5.00);
+var size= ["small", "medium", "large", "xl"]
+var pizza = [];
+var allToppings = [];
+
+// var cheese = new Pizza ("large", ["cheese"], 5.00);
+// var pepperoni = new Pizza ("large", ["cheese", "pepperoni"], 6.00);
+// var meats = new Pizza ("large", ["cheese", "pepperoni", "sausage", "bacon"], 8.00);
+// var supreme = new Pizza ("large", ["cheese", "pepperoni", "sausage", "bacon", "green peppers", "mushrooms", "olives"], 8.50);
+// var custom = new Pizza ("large", ["cheese"], 5.00);
 
 Pizza.prototype.priceSet = function() {
+  this.price = 7
+  if (this.size === "medium") {
+    this.price += 2;
+  }
+  if (this.size === "large") {
+    this.price += 3.75;
+  }
+  if (this.size === "xl") {
+    this.price += 5.25;
+  }
   return "$" + this.price.toFixed(2)
 }
+
+
+
 
 // USER INTERFACE LOGIC
 $(document).ready(function() {
